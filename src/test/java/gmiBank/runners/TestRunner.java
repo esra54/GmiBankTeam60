ff51bc5eccd8f85d895731c7c43e4cf4197bdd30
ufuktakmak <79896070+ufuktakmak@users.noreply.github.com>
2021-10-06 00:11:10 +0200

    ufuk

--- src/test/java/gmiBank/runners/TestRunner.java
+++ src/test/java/gmiBank/runners/TestRunner.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "gmiBank/stepDefinitions",
         tags = "@login",
+
         dryRun = false
 
 )
